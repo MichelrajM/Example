964f0a4615c7e9446fab821cbe315be7effa92f5
maikel <roshirosh0022gmail.com>
2024-09-05 04:55:30 +0000

    creating the directory

--- hello.sh
+++ hello.sh
@@ -7,8 +7,16 @@
 b=200
 echo $b
 
-
-
 a=100
 echo $a
+
+#declare the array
+array=("one" "two" "three")
+#
+#using for loop
+for dani in "${array[1]}";
+do
+	mkdir "$dani"
+	echo "created directory :$dani"
+done
 
